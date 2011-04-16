da73b6b4a0abf8c2f9bcebcdfd4b0437ff596b97
kiel.endpoint <kiel.endpoint@eadbbfa1-e406-518c-cd0f-e106bee6505e>
2011-04-16 20:33:42 +0000

    Only squidmon on master. Exclude drivers.ini from sync.
    git-svn-id: https://liquid-galaxy.googlecode.com/svn/trunk@283 eadbbfa1-e406-518c-cd0f-e106bee6505e

--- gnu_linux/home/lg/tools/squidmon.sh
+++ gnu_linux/home/lg/tools/squidmon.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+. ${HOME}/etc/shell.conf
+smallfile="http://www.endpoint.com/robots.txt"
+
 (echo -n "squidmon Start: "; date)>>/home/lg/tmp/squidmon.log
 
-smallfile="http://www.endpoint.com/robots.txt"
+# only run on the master system
+if [[ ${FRAME_NO:-1} -ne 0 ]]; then
+    echo "not master system. Exiting." >>/home/lg/tmp/squidmon.log
+    exit 1
+fi
 
 while :; do
     /home/lg/bin/lg-run "\
