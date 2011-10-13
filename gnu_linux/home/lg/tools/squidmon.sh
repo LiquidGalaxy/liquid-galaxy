fd851b2558f3ff80c3af8875854709e879a06835
kiel.endpoint <kiel.endpoint@eadbbfa1-e406-518c-cd0f-e106bee6505e>
2011-10-13 19:29:33 +0000

    squidmon: Use logger and add Squid PID age check.
    git-svn-id: https://liquid-galaxy.googlecode.com/svn/trunk@404 eadbbfa1-e406-518c-cd0f-e106bee6505e

--- gnu_linux/home/lg/tools/squidmon.sh
+++ gnu_linux/home/lg/tools/squidmon.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
+## GET CMDs
+# wget -q -t 1 -T 6 -O /dev/null --header='Host: www.endpoint.com' $SOMEFILE
+# curl -o /dev/null --no-keepalive -f -s -m 6 --retry 0 --header 'Host: www.endpoint.com' $SOMEFILE
+
 . ${HOME}/etc/shell.conf
 smallfile="http://www.endpoint.com/robots.txt"
+stampfile="/tmp/squidstamp"
+sqpidfile="/var/run/squid3.pid"
 
-(echo -n "squidmon Start: "; date)>>/home/lg/tmp/squidmon.log
+(echo -n "$0 Start: "; date) | logger -p local3.info
 
 # only run on the master system
 if [[ ${FRAME_NO:-1} -ne 0 ]]; then
-    echo "not master system. Exiting." >>/home/lg/tmp/squidmon.log
+    echo "$0: not master system. Exiting." | logger -p local3.err
     exit 1
 fi
 
 while :; do
-    /home/lg/bin/lg-run "\
-    if [[ -n \"\$( pgrep -f 'sbin\/squid3' )\" ]] \
-    && ( wget -q -t 1 -T 6 -O /dev/null --header='Host: www.endpoint.com' \"$smallfile\" ); then
-        echo -n \".squidok.\";
-     else
-     date >&2
-        echo \"\$(hostname).squidrestart.\" >&2;
-        ssh -i ~/.ssh/lg-id_rsa root@localhost service squid3 restart;
-     fi
-    " 2>>/home/lg/tmp/squidmon.log
+    2>&1 /home/lg/bin/lg-run "\
+    touch -t \"\$( date --date='5 minutes ago' +%y%m%d%H%M.%S )\" $stampfile;
+    if [[ -n \"\$( pgrep -f 'sbin\/squid3' )\" ]]; then
+        if ( wget -q -t 1 -T 6 -O /dev/null --header='Host: www.endpoint.com' \"$smallfile\" ); then
+            logger -p local3.info \".squidok.\";
+        elif [[ $sqpidfile -nt $stampfile ]]; then
+            logger -p local3.info \".waiting for squid to age.\";
+        else
+            logger -p local3.err \"\$(date +%s).\$(hostname).squidrestart.\";
+            ssh -i ~/.ssh/lg-id_rsa root@localhost \"service squid3 restart\" | logger -p local3.info;
+        fi
+    else
+        logger -p local3.err \"\$(date +%s).\$(hostname).squidrestart.\";
+        ssh -i ~/.ssh/lg-id_rsa root@localhost \"service squid3 restart\" | logger -p local3.info;
+    fi
+    " >/dev/null
     sleep 6
 done
