498ce06a2aa0016846a6ded4e8d45d918bc512c7
Kiel Christofferson <kiel@endpoint.com>
2011-11-07 15:05:46 -0500

    Further merge Googler Sean's touchscreen updates: * use JavaScript for feedback on KML load/unload * ensure a sub-menu always visible * rename layers png

--- php-interface/index.php
+++ php-interface/index.php
@@ -15,7 +15,7 @@
 
 include "sync_inc.php";
 
-$queries = array('earth' => array(), 'moon' => array(), 'mars' => array(), 'layers' => array());
+$queries = array('layers' => array(), 'earth' => array(), 'moon' => array(), 'mars' => array());
 $delimiter = "@";
 $handle = @fopen("queries.txt", "r");
 if ($handle) {
@@ -29,12 +29,13 @@ if ($handle) {
   fclose($handle);
 }
 
+#LG server
 $KML_SYS_PATH = '/var/www/kml/';
 $KML_SERVER_BASE = 'http://lg1:81/kml/';
 
 $FILE_FILTER = '*.km[l|z]';
 
-$kml_files = array('earth' => array(), 'moon' => array(), 'mars' => array());
+$kml_files = array('earth' => array(), 'moon' => array(), 'mars' => array(), 'layers' => array());
 foreach (array_keys($kml_files) as $planet) {
   $planet_kml_path = $KML_SYS_PATH . $planet . "/" . $FILE_FILTER;
   foreach (glob($planet_kml_path) as $file) {
@@ -44,6 +45,7 @@ foreach (array_keys($kml_files) as $planet) {
 }
 
 $kml_data_file = 'kmls.txt';
+
 $existing_kml_url_list = array_values(getKmlListUrls($kml_data_file));
 
 ?>
@@ -51,73 +53,65 @@ $existing_kml_url_list = array_values(getKmlListUrls($kml_data_file));
   <head>
     <link rel="stylesheet" type="text/css" href="style.css" />
     <script type="text/javascript" src="javascript.js"></script>
+
     <script type="text/javascript">       
       function clearKmls() {
         showAndHideStatus();
-        <?php  $i = 0; foreach ($kml_files['earth'] as $kml_name => $kml_file) { ?>
-          submitRequest('sync_touchscreen.php?touch_action=delete&touch_kml=<?php echo $kml_file; ?>');
+        <?php  $i = 0; foreach (array_values($queries['layers']) as $layer) { ?>
+          submitRequest('sync_touchscreen.php?touch_action=delete&touch_kml=<?php echo $layer[1]; ?>');
           document.getElementById('kml_<?php echo $i; ?>').className='kml_off';
         <?php ++$i; } ?>
         showAndHideStatus();
       }
     </script>
-    
+
     <title>Liquid Galaxy</title>
   </head>
-  <body>
+  <body onload="noneExpand('e_earth', 'e_layers', 'e_moon', 'e_mars', 'e_keyboard');">
     <div class="touchscreen">
       <div id="status"></div>
       <div class="earth">
-        <div class="title" onclick="changePlanet('earth');">Earth</div>
-        <img onclick="changePlanet('earth');" src="earth.png" />
-        <div class="expand">
+        <div class="title" onclick="changePlanet('earth');  toggleExpand('e_earth', 'e_layers', 'e_moon', 'e_mars', 'e_keyboard');">Earth</div>
+        <img onclick="changePlanet('earth');  toggleExpand('e_earth', 'e_layers', 'e_moon', 'e_mars', 'e_keyboard');" src="earth.png" />
+        <div class="expand_inactive" id="e_earth">
           <?php foreach (array_values($queries['earth']) as $query) { ?>
             <div class="location" onclick="changeQuery('<?php echo $query[1]; ?>', '<?php echo $query[0]; ?>');"><?php echo $query[0]; ?></div>
-          <? } ?>
+          <?php } ?>
         </div>
       </div>
       <div class="moon">
-        <div class="title" onclick="changePlanet('moon');">Moon</div>
-        <img onclick="changePlanet('moon');" src="moon.png" />
-        <div class="expand">
+        <div class="title" onclick="changePlanet('moon'); clearKmls(); toggleExpand('e_moon', 'e_earth', 'e_layers','e_mars', 'e_keyboard');">Moon</div>
+        <img onclick="changePlanet('moon'); clearKmls(); toggleExpand('e_moon', 'e_earth', 'e_layers','e_mars', 'e_keyboard');" src="moon.png" />
+        <div class="expand_inactive" id="e_moon">
           <?php foreach (array_values($queries['moon']) as $query) { ?>
             <div class="location" onclick="changeQuery('<?php echo $query[1]; ?>', '<?php echo $query[0]; ?>');"><?php echo $query[0]; ?></div>
-          <? } ?>
+          <?php } ?>
         </div>
       </div>
       <div class="mars">
-        <div class="title" onclick="changePlanet('mars');">Mars</div>
-        <img onclick="changePlanet('mars');" src="mars.png" />
-        <div class="expand">
+        <div class="title" onclick="changePlanet('mars'); clearKmls(); toggleExpand('e_mars', 'e_earth', 'e_layers','e_moon', 'e_keyboard');">Mars</div>
+        <img onclick="changePlanet('mars'); clearKmls(); toggleExpand('e_mars', 'e_earth', 'e_layers','e_moon', 'e_keyboard'); "  src="mars.png" />
+        <div class="expand_inactive" id="e_mars">
           <?php foreach (array_values($queries['mars']) as $query) { ?>
             <div class="location" onclick="changeQuery('<?php echo $query[1]; ?>', '<?php echo $query[0]; ?>');"><?php echo $query[0]; ?></div>
-          <? } ?>
+          <?php } ?>
         </div>
       </div>
       <div class="layers">
-        <div class="title" onclick="changePlanet('earth');">Data Layers</div>
-        <img src="gis_layers.png" />
-        <div class="expand">
-          <?php foreach (array_values($queries['layers']) as $layer) { ?>
-            <div class="location" onclick="changeLayer('<?php echo $layer[1]; ?>', '<?php echo $layer[0]; ?>');"><?php echo $layer[0]; ?></div>
-          <? } ?>
+        <div class="title" onclick="changePlanet('earth');  toggleExpand('e_layers', 'e_earth', 'e_mars','e_moon', 'e_keyboard');">Layers</div>
+        <img onclick="changePlanet('earth'); toggleExpand('e_layers', 'e_earth', 'e_moon', 'e_mars', 'e_keyboard');" src="layers.png" />
+        <div class="expand_inactive" id="e_layers">
+          <?php $i = 0; foreach (array_values($queries['layers']) as $layer) { ?>
+            <div id="kml_<?php echo $i; ?>" class="<?php echo (in_array($layer[1], $existing_kml_url_list)) ? "kml_on" : "kml_off"; ?>" onclick="toggleKml(this, '<?php echo $layer[1]; ?>');"><?php echo $layer[0]; ?></div>
+          <?php ++$i; } ?>
         </div>
       </div>
-      <!--// <div class="sview">
-        <div class="title" onclick="changeQuery('sview');">StreetView</div>
-        <img onclick="changeQuery('sview');" src="pegman.png" />
-        <div class="expand">
-          <?php #foreach (array_values($queries['sview']) as $query) { ?>
-            <div class="location" onclick="changeQuery('<?php echo $query[1]; ?>', '<?php echo $query[0]; ?>');"><?php echo $query[0]; ?></div>
-          <? #} ?>
-        </div>
-      </div> //-->
       <div class="keyboard">
-        <div class="title">&nbsp;</div>
-        <img src="keyboard.png" />
-        <div class="expand">
+        <div class="title" onclick="toggleExpand('e_keyboard', 'e_earth', 'e_mars','e_moon', 'e_layers');">&nbsp;</div>
+        <img onclick="toggleExpand('e_keyboard', 'e_earth', 'e_mars','e_moon', 'e_layers');" src="keyboard.png" />
+        <div class="expand_inactive" id="e_keyboard">
           <div class="keyboardRow1">
-            <input class="keyboardEntry" id="keyboardEntry" name="keyboardEntry" type="text" value=""/>
+            <input class="keyboardEntry" id="keyboardEntry" name="keyboardEntry" type="text" value="" onkeypress="enterKeySubmit(event);" />
             <div class="keyboardKey keyboardKeyClear" onclick='clearKey();'>Clear</div>
             <div class="keyboardKey keyboardKeySearch" onclick='searchKey();'>Search</div>
           </div>
@@ -147,10 +141,10 @@ $existing_kml_url_list = array_values(getKmlListUrls($kml_data_file));
           </div>
         </div>
       </div>
-      <div class="welcome">
+      <div class="welcome" onclick="noneExpand('e_earth', 'e_layers', 'e_moon', 'e_mars', 'e_keyboard');">
         Welcome to the Liquid Galaxy by Google
       </div>
-	<!--//
+      <!--//
       <div class="ctlpanel">
         <div class="appctl">
           <div class="title" onclick="changeQuery('sview', 'StreetView');">StreetView</div>
@@ -172,7 +166,7 @@ $existing_kml_url_list = array_values(getKmlListUrls($kml_data_file));
           <div class="ctlbutton" onclick="changeQuery('mpctl-stop', 'MP STOP');">STOP</div>
         </div>
       </div>
-	//-->
+      //-->
       <div class="relaunch">
         <div class="location" onclick="changeQuery('relaunch', 'Relaunch');">Relaunch</div>
       </div>
--- php-interface/javascript.js
+++ php-interface/javascript.js
@@ -162,9 +162,17 @@ function enterKeySubmit(e) {
   }
 }
 
-function toggleExpand(on_obj, off_obj1, off_obj2, off_obj3){
+function toggleExpand(on_obj, off_obj1, off_obj2, off_obj3, off_obj4){
   document.getElementById(on_obj).className='expand_active';
   document.getElementById(off_obj1).className='expand_inactive';
   document.getElementById(off_obj2).className='expand_inactive';
   document.getElementById(off_obj3).className='expand_inactive';
+  document.getElementById(off_obj4).className='expand_inactive';
+}
+function noneExpand(off_obj, off_obj1, off_obj2, off_obj3, off_obj4){
+  document.getElementById(off_obj).className='expand_inactive';
+  document.getElementById(off_obj1).className='expand_inactive';
+  document.getElementById(off_obj2).className='expand_inactive';
+  document.getElementById(off_obj3).className='expand_inactive';
+  document.getElementById(off_obj4).className='expand_inactive';
 }
