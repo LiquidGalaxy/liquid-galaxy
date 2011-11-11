cff0d515c9b92ec343e15a27e552f40eebdf1900
Kiel Christofferson <kiel@endpoint.com>
2011-11-11 18:14:03 -0500

    Use KML from earth/kml. Process m4 conf. * Multiple-screen support in run-earth-bin and write-drivers * Utilize "EARTH_QUERY" variable * More effective pid killing in run-earth-bin

--- earth/scripts/run-earth-bin.sh
+++ earth/scripts/run-earth-bin.sh
@@ -15,50 +15,78 @@
 
 . ${HOME}/etc/shell.conf
 
-echo "$(date +%s) $0 running."
+logger -p local3.info -i "$0: running at $(date +%s)"
 
 # kill any other copies
 ME="`basename $0`"
+MEPIDS="$( pidof -x -o '%PPID' $ME )"
+for pid in ${MEPIDS}; do
+    pkill -u $(id -u) $pid
+done
 
-pkill `pidof -x -o '%PPID' $ME`
-
-killall googleearth-bin
+pkill -u $(id -u) googleearth-bin
 sleep 2
 
-export DISPLAY=:0.0
+if [[ "$( id -un )" == "lg" ]]; then
+    for screen in /home/lgS*; do
+        if [[ -d ${screen} ]]; then
+            screennum=${screen##/home/lgS}
+            logger -p local3.info -i "$0: launching $ME for my screen \"${screennum}\""
+            sudo -u lgS${screennum} -H DISPLAY=:0.${screennum} ${SCRIPDIR}/${ME} ${@} &
+            unset screennum
+        fi
+    done
+fi
+
+[[ -n "${DISPLAY}" ]] || export DISPLAY=:0.0
+[ ${DISPLAY##*\.} -ne 0 ] && export SCREEN_NO=${DISPLAY##*\.}
 export __GL_SYNC_TO_VBLANK=1  # broken for nvidia when rotating screen
 
 cd ${SCRIPDIR} || exit 1
-echo "running write drivers"
+logger -p local3.info -i "$0: running write drivers. S:\"${SCREEN_NO}\"."
 ./write-drivers-ini.sh
 
-if [[ "`cat /lg/frame`" -eq 0 ]] ; then
+if [ $FRAME_NO -eq 0 ] ; then
     DIR=master
 else
     DIR=slave
 fi
-cp -a ${EARTHDIR}/config/$DIR/* ${HOME}/.config/Google/
-cp -a ${EARTHDIR}/config/myplaces.kml ${HOME}/.googleearth/
+
+MYCFGDIR="${CONFGDIR}/${DIR}"
+# build the configuration file
+m4 -I${MYCFGDIR} ${MYCFGDIR}/GECommonSettings.conf.m4 > ${MYCFGDIR}/$( basename `readlink ${MYCFGDIR}/GECommonSettings.conf.m4` .m4 )
+# copying files AND potentially symlinks here
+cp -a ${MYCFGDIR}/*        ${HOME}/.config/Google/
+cp -a ${LGKMLDIR}/${DIR}/* ${HOME}/.googleearth/
+# expand the ##HOMEDIR## var in configs
+sed -i -e "s:##HOMEDIR##:${HOME}:g" ${HOME}/.config/Google/*.conf
+# expand LG_PHPIFACE (may contain ":" and "/") in kml files
+sed -i -e "s@##LG_PHPIFACE##@${LG_PHPIFACE}@g" ${HOME}/.googleearth/*.kml
   
 while true ; do
     if [[ "$DIR" == "master" ]]; then
-        lg-run killall googleearth-bin
+        lg-sudo killall googleearth-bin
     fi
     [ -w $SPACENAVDEV ] && ${HOME}/bin/led-enable ${SPACENAVDEV} 1
 
-    cd ${EARTHDIR}/builds/latest
+    cd ${BUILDDIR}/${EARTH_BUILD}
     rm -f ${HOME}/.googleearth/Cache/db* # important: otherwise we get random broken tiles
     rm -rf ${HOME}/.googleearth/Temp/*
-    rm -f /tmp/query.txt
-    # push mouse over to touchscreen interface
-    DISPLAY=:0 xdotool mousemove -screen 1 1910 1190
-    echo "running earth"
-#    PLANET="`cat /lg/planet`"
-#    ${SCRIPDIR}/set-planet.sh $PLANET
-    # seems that GE Qt libraries were build without fontconfig support
-    # use "-font" to specify something _just_ for the Qt menus
-    ./googleearth -style cleanlooks --fullscreen -font '-adobe-helvetica-bold-r-normal-*-9-*-*-*-p-*-iso8859-1'
+    rm -f ${EARTH_QUERY:-/tmp/query.txt}
+    # shove mouse over to touchscreen interface
+    if [[ "$DIR" == "master" ]]; then
+        # use the touchscreen
+        DISPLAY=:0 xdotool mousemove -screen 1 1910 1190
+    else
+        # lock the keyboard and mouse
+        DISPLAY=:0 xtrlock & DISPLAY=:0 xdotool mousemove -screen 0 1190 1910
+    fi
+    logger -p local3.info -i "$0: running earth"
+    ./googleearth -style cleanlooks --fullscreen -font '-adobe-helvetica-bold-r-normal-*-3-*-*-*-p-*-iso8859-1'
+    # Normally use TINY font size to make the menu bar small and unobtrusive, but error windows become unreadable.
+    # use the below execution for large font. (qt4 is supposed to ignore '-font' if built with freetype support).
+    #./googleearth -style cleanlooks --fullscreen -font '-adobe-helvetica-bold-r-normal-*-16-*-*-*-p-*-iso8859-1'
 
     [ -w $SPACENAVDEV ] && ${HOME}/bin/led-enable ${SPACENAVDEV} 0
-    sleep 5
+    sleep 3
 done
--- earth/scripts/write-drivers-ini.sh
+++ earth/scripts/write-drivers-ini.sh
@@ -33,10 +33,25 @@ FOV="36.5"
 YAW_AMOUNT="-42"
 
 YAW="$(echo $FRAME_NO '*' $YAW_AMOUNT | bc)"
+# adjust YAW for secondary (further away from LG1) screens
+if [[ -n "${SCREEN_NO}" ]]; then
+    logger -p local3.info "$0: have screen number \"${SCREEN_NO}\", adjusting YAW"
+    if [[ $FRAME_NO -gt 0 ]]; then
+        let "YAW += $( echo $YAW_AMOUNT '*' $SCREEN_NO | bc -l )"
+    elif [[ $FRAME_NO -lt 0 ]]; then
+        let "YAW -= $( echo $YAW_AMOUNT '*' $SCREEN_NO | bc -l )"
+    fi  
+fi
 MYIPALIAS="$( awk '/^ifconfig/ {print $3}' /etc/network/if-up.d/*-lg_alias )"
 VSYNCCHOP="${MYIPALIAS%.*}"
 VSYNCHOST="10.42.${VSYNCCHOP##*.}.255"
 VSYNCPORT="$EARTH_PORT"
+
+# Adjust ViewSync packet destination if using a ViewSync relay
+if [[ "$VSYNC_RELAY" == "true" ]] && [[ "$MASTER" == "true" ]]; then
+    VSYNCHOST=127.0.0.1
+    VSYNCPORT=$((${VSYNCPORT}-1))
+fi
 
 cd ${EARTHDIR} || exit 1
 
@@ -47,10 +62,11 @@ echo "VSYNCPORT: $VSYNCPORT"
 echo "YAW: $YAW"
 echo "FOV: $FOV"
 echo "NAV: $SPACENAVDEV"
+echo "QUERY: $EARTH_QUERY"
 
 chmod 644 builds/latest/drivers.ini
 
-# remember the navigator device will have "/"
+# remember the navigator device AND query file path will have "/" in them
 cat ${EARTHDIR}/config/drivers_template.ini |\
   sed -e "s/##MASTER##/$MASTER/" \
   -e "s/##SLAVE##/$SLAVE/" \
@@ -58,5 +74,6 @@ cat ${EARTHDIR}/config/drivers_template.ini |\
   -e "s/##VSYNCPORT##/$VSYNCPORT/" \
   -e "s/##YAW##/$YAW/" \
   -e "s/##FOV##/$FOV/" \
+  -e "s:##EARTH_QUERY##:$EARTH_QUERY:" \
   -e "s:##NAV##:$SPACENAVDEV:" > builds/latest/drivers.ini
 
