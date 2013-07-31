23e0ec3ac8730e3d915113063630ee9dde12857f
Joshua Tolley <josh@endpoint.com>
2013-07-31 15:41:22 -0600

    Beginning use of parsers, to make a tour multicamera-ready automatically

---
+++ kamelopard/lib/kamelopard/multicamify.rb
@@ -0,0 +1,51 @@
+#!env ruby
+
+# The idea here is to ingest one tour from one KML document, and make it
+# multicamera-ish
+
+#require 'rubygems'
+$LOAD_PATH << './lib'
+require 'kamelopard'
+require 'libxml'
+
+# command line options: a single kml file. Prints KML to stdout
+
+def process_wait(w)
+    Kamelopard::Wait.parse(w)
+end
+
+def process_flyto(f)
+    # The idea here is to check the flytomode. If it's "smooth", just fly to
+    # that point. If it's "bounce", use bounce() to simulate the flyto. Since
+    # bounce() requires a start and an end, we'll need to keep track of where
+    # we are, so if the next flyto is a bounce, we have a start point.
+    # XXX Question: What if the *first* flyto is a bounce? We won't have a
+    # start point. Throw an error?
+    # XXX Should we assume a default value for flyToMode, if we don't find one?
+    f.find('//gx:flyToMode').each do |m|
+        if m.children[0].to_s == 'smooth'
+        break
+    end
+end
+
+d = XML::Document.file(ARGV[0])
+tours = d.find('//gx:Tour')
+if tours.size > 1 then
+    STDERR.puts "Found multiple tours in this document. Processing only the first."
+    # XXX Fix this
+elsif tours.size == 0 then
+    STDERR.puts "Found no tours in the document. Error."
+    exit 1
+end
+
+tour = tours[0]
+
+tour.find('//gx:FlyTo|//gx:Wait').each do |n|
+    if n.name == 'Wait'
+        Kamelopard::Wait.parse(w)
+    elsif n.name == 'FlyTo'
+        process_flyto n
+    end
+end
+
+puts get_kml.to_s
