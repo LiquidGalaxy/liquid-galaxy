1b84d9fbab245ddc421d5d9f7a295b0eea6cfc85
Joshua Tolley <josh@endpoint.com>
2014-05-05 09:37:55 -0600

    Whitespace cleanup

--- kamelopard/lib/kamelopard/helpers.rb
+++ kamelopard/lib/kamelopard/helpers.rb
@@ -7,7 +7,7 @@
   def get_document()
       Kamelopard::DocumentHolder.instance.current_document
   end
-  
+
   # Changes the default FlyTo mode. Possible values are :smooth and :bounce
   def set_flyto_mode_to(mode)
       Kamelopard::DocumentHolder.instance.current_document.flyto_mode = mode
@@ -19,7 +19,7 @@
         dh.delete_current_doc while dh.documents.size > 0
     end
 
-  
+
   # Shows or hides the popup balloon for Placemark and ScreenOverlay objects.
   # Arguments are the object; 0 or 1 to hide or show the balloon, respectively;
   # and a hash of options to be added to the AnimatedUpdate object this
@@ -44,7 +44,7 @@
       a << b
       au << a
   end
-  
+
   # Hides the popup balloon for a Placemark or ScreenOverlay object. Require
   # the object as the first argument, and takes a hash of options passed to the
   # AnimatedUpdate object this functino creates. See also show_balloon_for and
@@ -52,7 +52,7 @@
   def hide_balloon_for(obj, options = {})
       toggle_balloon_for(obj, 0, options)
   end
-  
+
   # Displays the popup balloon for a Placemark or ScreenOverlay object. Require
   # the object as the first argument, and takes a hash of options passed to the
   # AnimatedUpdate object this functino creates. See also show_balloon_for and
@@ -60,7 +60,7 @@
   def show_balloon_for(obj, options = {})
       toggle_balloon_for(obj, 1, options)
   end
-  
+
   # Fades a placemark's popup balloon in or out. Takes as arguments the
   # placemark object, 0 or 1 to hide or show the balloon, respectively, and a
   # has of options to be passed to the AnimatedUpdate object created by this
@@ -81,79 +81,79 @@
       a << b
       au << a
   end
-  
+
   # Refer to fade_balloon_for. This function only fades the balloon out.
   def fade_out_balloon_for(obj, options = {})
       fade_balloon_for(obj, '00ffffff', options)
   end
-  
+
   # Refer to fade_balloon_for. This function only fades the balloon in.
   def fade_in_balloon_for(p, options = {})
       fade_balloon_for(p, 'ffffffff', options)
   end
-  
+
   # Creates a Point object. Arguments are latitude, longitude, altitude,
   # altitude mode, and extrude
   def point(lo, la, alt=0, mode=nil, extrude = false)
       m = ( mode.nil? ? :clampToGround : mode )
       Kamelopard::Point.new(lo, la, alt, :altitudeMode => m, :extrude => extrude)
   end
-  
+
   # Creates a Placemark with the given name. Other Placemark attributes are set
   # in the options hash.
   def placemark(name = nil, options = {})
       Kamelopard::Placemark.new name, options
   end
-  
+
   # Returns the KML that makes up the current Kamelopard::Document
   def get_kml
       Kamelopard::DocumentHolder.instance.current_document.get_kml_document
   end
-  
+
   # Returns the KML that makes up the current Document, as a string
   def get_kml_string
       get_kml.to_s
   end
-  
+
   # Inserts a KML gx:Wait element
   def pause(p, options = {})
       Kamelopard::Wait.new p, options
   end
-  
+
   # Returns the current Tour object
   def get_tour()
       Kamelopard::DocumentHolder.instance.current_document.tour
   end
-  
+
   # Sets a name for the current Tour
   def name_tour(name)
       Kamelopard::DocumentHolder.instance.current_document.tour.name = name
   end
-  
+
   # Returns the current Folder object
   def get_folder()
       f = Kamelopard::DocumentHolder.instance.current_document.folders.last
       Kamelopard::Folder.new() if f.nil?
       Kamelopard::DocumentHolder.instance.current_document.folders.last
   end
-  
+
   # Creates a new Folder with the current name
   def folder(name)
       Kamelopard::Folder.new(name)
   end
-  
+
   # Names (or renames) the current Folder, and returns it
   def name_folder(name)
       Kamelopard::DocumentHolder.instance.current_document.folder.name = name
       return Kamelopard::DocumentHolder.instance.current_document.folder
   end
-  
+
   # Names (or renames) the current Document object, and returns it
   def name_document(name)
       Kamelopard::DocumentHolder.instance.current_document.name = name
       return Kamelopard::DocumentHolder.instance.current_document
   end
-  
+
   def zoom_out(dist = 1000, dur = 0, mode = nil)
       l = Kamelopard::DocumentHolder.instance.current_document.tour.last_abs_view
       raise "No current position to zoom out from\n" if l.nil?
@@ -173,12 +173,12 @@
         end
         heading
     end
-  
+
     # Creates a list of FlyTo elements to orbit and look at a given point (center),
     # at a given range (in meters), starting and ending at given angles (in
     # degrees) from the center, where 0 and 360 (and -360, and 720, and -980, etc.)
     # are north. To orbit multiple times, add or subtract 360 from the
-    # endHeading. 
+    # endHeading.
     # The tilt argument matches the KML LookAt tilt argument
     # already_there, if true, means we've already flown to the initial point
     # The options hash can contain:
@@ -192,16 +192,16 @@
         already_there = options.has_key?(:already_there) ? options[:already_there] : false
 
         am = center.altitudeMode
-  
+
         if not step.nil? then
             if (endHeading - startHeading > 0 and step < 0) or (endHeading - startHeading < 0 and step > 0) then
                 raise "Given start = #{startHeading}, end = #{endHeading}, and step = #{step}, this will be an infinite loop"
             end
         end
-    
+
         # We want at least 5 points (arbitrarily chosen value), plus at least 5 for
         # each full revolution
-    
+
         # When I tried this all in one step, ruby told me 360 / 10 = 1805. I'm sure
         # there's some reason why this is a feature and not a bug, but I'd rather
         # not look it up right now.
@@ -223,7 +223,7 @@
           dur = duration * 1.0 / ((endHeading - startHeading) * 1.0 / step) if duration != 0
           startHeading = startHeading + step if already_there
         end
-    
+
         lastval = startHeading
         mode = :bounce
         mode = :smooth if already_there
@@ -237,43 +237,43 @@
             fly_to Kamelopard::LookAt.new(center, :heading => convert_heading(endHeading), :tilt => tilt, :range => range, :altitudeMode => am), :duration => dur, :mode => :smooth
         end
     end
-    
+
     # Adds a SoundCue object.
     def sound_cue(href, ds = nil)
         Kamelopard::SoundCue.new href, ds
     end
-  
+
   # XXX This implementation of orbit is trying to do things the hard way, but the code might be useful for other situations where the hard way is the only possible one
   # def orbit(center, range = 100, startHeading = 0, endHeading = 360)
   #     p = ThreeDPointList.new()
-  # 
+  #
   #     # Figure out how far we're going, and d
   #     dist = endHeading - startHeading
-  # 
+  #
   #     # We want at least 5 points (arbitrarily chosen value), plus at least 5 for each full revolution
   #     step = (endHeading - startHeading) / ((endHeading - startHeading) / 360.0).to_i * 5 + 5
   #     startHeading.step(endHeading, step) do |theta|
   #         p << KMLPoint.new(
-  #             center.longitude + Math.cos(theta), 
-  #             center.latitude + Math.sin(theta), 
+  #             center.longitude + Math.cos(theta),
+  #             center.latitude + Math.sin(theta),
   #             center.altitude, center.altitudeMode)
   #     end
   #     p << KMLPoint.new(
-  #         center.longitude + Math.cos(endHeading), 
-  #         center.latitude + Math.sin(endHeading), 
+  #         center.longitude + Math.cos(endHeading),
+  #         center.latitude + Math.sin(endHeading),
   #         center.altitude, center.altitudeMode)
-  # 
+  #
   #     p.interpolate.each do |a|
-  #         fly_to 
+  #         fly_to
   #     end
   # end
-  
+
   # Sets a prefix for all kml_id objects. Note that this does *not* change
   # previously created objects' kml_ids... just new kml_ids going forward.
   def set_prefix_to(a)
       Kamelopard.id_prefix = a
   end
-  
+
   # Writes KML output (and if applicable, viewsyncrelay configuration) to files.
   # Include a file name for the actions_file argument to get viewsyncrelay
   # configuration output as well. Note that this configuration includes only the
@@ -287,7 +287,7 @@
       end
       #File.open(file, 'w') do |f| f.write get_kml.to_s.gsub(/balloonVis/, 'gx:balloonVis') end
   end
-  
+
   # Fades a screen overlay in or out. The show argument is boolean; true to
   # show the overlay, or false to hide it. The fade will happen smoothly (as
   # opposed to immediately) if the options hash includes a :duration element
@@ -296,11 +296,11 @@
       color = '00ffffff'
       color = 'ffffffff' if show
       if ov.is_a? String then
-          id = ov  
+          id = ov
       else
           id = ov.kml_id
       end
-  
+
       a = XML::Node.new 'Change'
       b = XML::Node.new 'ScreenOverlay'
       b.attributes['targetId'] = id
@@ -308,32 +308,32 @@
       c << XML::Node.new_text(color)
       b << c
       a << b
-      k = Kamelopard::AnimatedUpdate.new [a], options 
+      k = Kamelopard::AnimatedUpdate.new [a], options
   end
-  
+
   # Given telemetry data, such as from an aircraft, including latitude,
   # longitude, and altitude, this will figure out realistic-looking tilt,
   # heading, and roll values and create a series of FlyTo objects to follow the
   # extrapolated flight path
   module TelemetryProcessor
       Pi = 3.1415926535
-  
+
       def TelemetryProcessor.get_heading(p)
           x1, y1, x2, y2 = [ p[1][0], p[1][1], p[2][0], p[2][1] ]
-  
+
           h = Math.atan((x2-x1) / (y2-y1)) * 180 / Pi
           h = h + 180.0 if y2 < y1
           h
       end
-  
+
       def TelemetryProcessor.get_dist2(x1, y1, x2, y2)
           Math.sqrt( (x2 - x1)**2 + (y2 - y1)**2).abs
       end
-  
+
       def TelemetryProcessor.get_dist3(x1, y1, z1, x2, y2, z2)
           Math.sqrt( (x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2 ).abs
       end
-  
+
       def TelemetryProcessor.get_tilt(p)
           x1, y1, z1, x2, y2, z2 = [ p[1][0], p[1][1], p[1][2], p[2][0], p[2][1], p[2][2] ]
           smoothing_factor = 10.0
@@ -344,33 +344,33 @@
                   # the / 2.0 is just because it looked nicer that way
           90.0 + t
       end
-  
+
       def TelemetryProcessor.get_roll(p)
           x1, y1, x2, y2, x3, y3 = [ p[0][0], p[0][1], p[1][0], p[1][1], p[2][0], p[2][1] ]
           return 0 if x1.nil? or x2.nil?
-  
+
           # Measure roll based on angle between P1 -> P2 and P2 -> P3. To be really
           # exact I ought to take into account altitude as well, but ... I don't want
           # to
-  
+
           # Set x2, y2 as the origin
           xn1 = x1 - x2
           xn3 = x3 - x2
           yn1 = y1 - y2
           yn3 = y3 - y2
-          
+
           # Use dot product to get the angle between the two segments
           angle = Math.acos( ((xn1 * xn3) + (yn1 * yn3)) / (get_dist2(0, 0, xn1, yn1).abs * get_dist2(0, 0, xn3, yn3).abs) ) * 180 / Pi
-  
+
           @@options[:exaggerate] * (angle - 180)
       end
-  
+
       def TelemetryProcessor.fix_coord(a)
           a = a - 360 if a > 180
           a = a + 360 if a < -180
           a
       end
-  
+
       # This is the only function in the module that users are expected to
       # call, and even then users should probably use the tour_from_points
       # function. The p argument contains an ordered array of points, where
@@ -394,33 +394,33 @@
           f = Kamelopard::FlyTo.new c, { :duration => @@options[:pause], :mode => :smooth }
           f.comment = "#{p[1][0]} #{p[1][1]} #{p[1][2]} to #{p[2][0]} #{p[2][1]} #{p[2][2]}"
       end
-  
+
       def TelemetryProcessor.options=(a)
           @@options = a
       end
-  
+
       def TelemetryProcessor.normalize_points(p)
           # The whole point here is to prevent problems when you cross the poles or the dateline
           # This could have serious problems if points are really far apart, like
           # hundreds of degrees. This seems unlikely.
           lons = ((0..2).collect { |i| p[i][0] })
           lats = ((0..2).collect { |i| p[i][1] })
-  
+
           lon_min, lon_max = lons.minmax
           lat_min, lat_max = lats.minmax
-  
+
           if (lon_max - lon_min).abs > 200 then
               (0..2).each do |i|
                   lons[i] += 360.0 if p[i][0] < 0
               end
           end
-  
+
           if (lat_max - lat_min).abs > 200 then
               (0..2).each do |i|
                   lats[i] += 360.0 if p[i][1] < 0
               end
           end
-  
+
           return [
               [ lons[0], lats[0], p[0][2] ],
               [ lons[1], lats[1], p[1][2] ],
@@ -428,7 +428,7 @@
           ]
       end
   end
-  
+
   # Creates a tour from a series of points, using TelemetryProcessor::add_flyto.
   #
   # The first argument is an ordered array of points, where each point is
@@ -450,7 +450,7 @@
           TelemetryProcessor::add_flyto points[i,3]
       end
   end
-  
+
   # Given a hash of values, this creates an AbstractView object. Possible
   # values in the hash are :latitude, :longitude, :altitude, :altitudeMode,
   # :tilt, :heading, :roll, :range, :begin, :end, and :when. If the hash
@@ -462,14 +462,14 @@
   #
   # :roll, :range, and the timestamp / timespan options have no default; all
   # other values default to 0 except :altitudeMode, which defaults to
-  # :relativeToGround. 
+  # :relativeToGround.
   def make_view_from(options = {})
       o = {}
       o.merge! options
       options.each do |k, v|
           o[k.to_sym] = v unless k.kind_of? Symbol
       end
-  
+
       # Set defaults
       [
           [ :altitude, 0 ],
@@ -482,9 +482,9 @@
       ].each do |a|
           o[a[0]] = a[1] unless o.has_key? a[0]
       end
-  
+
       p = point o[:longitude], o[:latitude], o[:altitude], o[:altitudeMode], o[:extrude]
-  
+
       if o.has_key? :roll then
           view = Kamelopard::Camera.new p
       else
@@ -499,55 +499,55 @@
           e = o[:end] if o.has_key? :end
           o[:timespan] = Kamelopard::TimeSpan.new(b, e)
       end
-  
+
       [ :altitudeMode, :tilt, :heading, :timespan, :timestamp, :range, :roll, :viewerOptions ].each do |a|
           #p o[a] if o.has_key? a and a == :timestamp
           view.method("#{a.to_s}=").call(o[a]) if o.has_key? a
       end
-  
+
       view
   end
-  
+
   # Creates a ScreenOverlay object
   def screenoverlay(options = {})
       Kamelopard::ScreenOverlay.new options
   end
-  
+
   # Creates an XY object, for use when building Overlay objects
   def xy(x = 0.5, y = 0.5, xt = :fraction, yt = :fraction)
       Kamelopard::XY.new x, y, xt, yt
   end
-  
+
   # Creates an IconStyle object.
   def iconstyle(href = nil, options = {})
       Kamelopard::IconStyle.new href, options
   end
-  
+
   # Creates an LabelStyle object.
   def labelstyle(scale = 1, options = {})
       Kamelopard::LabelStyle.new scale, options
   end
-  
+
   # Creates an BalloonStyle object.
   def balloonstyle(text, options = {})
       Kamelopard::BalloonStyle.new text, options
   end
-  
+
   # Creates an Style object.
   def style(options = {})
       Kamelopard::Style.new options
   end
-  
+
   # Creates a LookAt object focused on the given point
   def look_at(point = nil, options = {})
       Kamelopard::LookAt.new point, options
   end
-  
+
   # Creates a Camera object focused on the given point
   def camera(point = nil, options = {})
       Kamelopard::Camera.new point, options
   end
-  
+
   # Creates a FlyTo object flying to the given AbstractView
   def fly_to(view = nil, options = {})
       Kamelopard::FlyTo.new view, options
@@ -558,7 +558,7 @@
   def each_flyto(d)
       d.find('//gx:FlyTo').each do |p|
           all_values = {}
-  
+
           # These fields are part of the abstractview
           view_fields = %w{ latitude longitude heading range tilt roll altitude altitudeMode gx:altitudeMode coordinates}
           # These are other field I'm interested in
@@ -572,7 +572,7 @@
               else
                   ix = "kml:#{k}"
               end
-              r = k == "gx:altitudeMode" ? :altitudeMode : k.to_sym 
+              r = k == "gx:altitudeMode" ? :altitudeMode : k.to_sym
               tmp = p.find_first("descendant::#{ix}")
               next if tmp.nil?
               all_values[k == "gx:altitudeMode" ? :altitudeMode : k.to_sym ] = tmp.content
@@ -582,14 +582,14 @@
           yield make_view_from(view_values), all_values
       end
   end
-  
+
   # Pulls the Placemarks from the KML document d and yields each in turn to the caller
   # d = an XML::Document containing KML
   # XXX This currently expects Placemarks to contain LookAt or Camera objects. It should do something useful with placemarks based on Points, or other objects
   def each_placemark(d)
       d.find('//kml:Placemark').each do |p|
           all_values = {}
-  
+
           # These fields are part of the abstractview
           view_fields = %w{ latitude longitude heading range tilt roll altitude altitudeMode gx:altitudeMode }
           # These are other field I'm interested in
@@ -603,7 +603,7 @@
               else
                   ix = "kml:#{k}"
               end
-              r = k == "gx:altitudeMode" ? :altitudeMode : k.to_sym 
+              r = k == "gx:altitudeMode" ? :altitudeMode : k.to_sym
               tmp = p.find_first("descendant::#{ix}")
               next if tmp.nil?
               all_values[k == "gx:altitudeMode" ? :altitudeMode : k.to_sym ] = tmp.content
@@ -613,34 +613,34 @@
           yield make_view_from(view_values), all_values
       end
   end
-  
+
   # Makes an HTML tour index, linked to a one-pixel screen overlay. The HTML
   # contains links to start each tour.
   def make_tour_index(erb = nil, options = {})
       get_document.make_tour_index(erb, options)
   end
-  
+
   # Superceded by toggle_balloon_for, but retained for backward compatibility
   def show_hide_balloon(p, wait, options = {})
       show_balloon_for p, options
       pause wait
       hide_balloon_for p, options
   end
-  
+
   # Creates a CDATA XML::Node. This is useful for, among other things,
   # ExtendedData values
   def cdata(text)
       XML::Node.new_cdata text.to_s
   end
-  
+
   # Returns an array of two values, equal to l +/- p%, defining a "band" around the central value l
   # NB! p is interpreted as a percentage, not a fraction. IOW the result is divided by 100.0.
   def band(l, p)
       f = l * p / 100.0
       [ l - f, l + f ]
   end
-  
-  
+
+
   # Ensures v is within the range [min, max]. Modifies v to be within that range,
   # assuming the number line is circular (as with latitude or longitude)
   def circ_bounds(v, max, min)
@@ -656,13 +656,13 @@
       end
       v
   end
-  
+
   # These functions ensure the given value is within appropriate bounds for a
   # latitude or longitude. Modifies it as necessary if it's not.
   def lat_check(l)
       circ_bounds(l * 1.0, 90.0, -90.0)
   end
-  
+
   # See lat_check()
   def long_check(l)
       circ_bounds(l * 1.0, 180.0, -180.0)
@@ -673,7 +673,7 @@
   def to_constraint(arr)
     "[#{arr[0]}, #{arr[1]}]"
   end
-  
+
   # Adds a VSRAction object (a viewsyncrelay action) to the document, for
   # viewsyncrelay configuration
   def do_action(cmd, options = {})
@@ -763,12 +763,12 @@
         radius = 6371  # rough radius of the Earth, in kilometers
         lat1, long1 = [Math::PI * a.latitude / 180.0, Math::PI * a.longitude / 180.0]
         lat2, long2 = [Math::PI * b.latitude / 180.0, Math::PI * b.longitude / 180.0]
-        d = 2 * radius * 
+        d = 2 * radius *
             Math.asin( Math.sqrt(
                 Math.sin((lat2-lat1)/2)**2 +
                 Math.cos(lat1) * Math.cos(lat2) *
                 Math.sin((long2 - long1)/2)**2
             ))
-       
+
         return d
     end
